b7b274bf2755386938e06d48e3e368c368e08de0
TH779 <i@779.moe>
2021-10-14 15:37:47 +0000

    sdm710-common: Uprev fingerprint HAL to 2.3

--- sdm710.mk
+++ sdm710.mk
@@ -186,7 +186,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm710
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sdm710
 
 ifeq ($(TARGET_HAS_FOD),true)
 PRODUCT_PACKAGES += \
